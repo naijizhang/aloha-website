b297ea8a61c5409f1630f9d6af65523c36f60bb8
Naiji Zhang <naijizhang@gmail.com>
2019-01-18 14:37:21 -0800

    Update Format and Flickity

--- js/index.js
+++ js/index.js
@@ -13,7 +13,9 @@ $(function() {
 $(".main-carousel").flickity({
   // options
   cellAlign: "left",
-  contain: true
+  contain: true,
+  imagesLoaded: true,
+  prevNextButtons: false,
 });
 
 // reference: https://stackoverflow.com/questions/2507030/email-validation-using-jquery
@@ -21,3 +23,21 @@ function isEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
 }
+
+//smooth scrolling
+// @source https://css-tricks.com/smooth-scrolling-accessibility/
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  
