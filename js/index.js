b845cf32c6eb1dd2e5dcc8fe9afab06eb96b24f2
Naiji Zhang <naijizhang@gmail.com>
2019-01-18 12:47:38 -0800

    Update Format and Flickity

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
-$(function(){
-    $("button").on("click", function(){
-        let text= $('input[class="text"]').val();
-       
-        if(text===""){
-            alert("Please enter email address.");
-        } else{
-            alert("Thanks for subscribing.");
-        }
-    });
-
+$(function() {
+  $("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    let text = $('input[class="text"]').val();
+    if (isEmail(text)) {
+      alert("Thanks for subscribing.");
+    } else {
+      alert("The email address is invalid.");
+    }
+  });
 });
+
+$(".main-carousel").flickity({
+  // options
+  cellAlign: "left",
+  contain: true
+});
+
+// reference: https://stackoverflow.com/questions/2507030/email-validation-using-jquery
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
